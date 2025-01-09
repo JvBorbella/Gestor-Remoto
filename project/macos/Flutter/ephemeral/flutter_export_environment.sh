489949676e1a443332de06f67c83e4e9f1dac58e
João Victor Borbella <116276996+JvBorbella@users.noreply.github.com>
2025-01-09 16:38:50 -0300

    Att 09/01 - 3
    😞

--- project/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ project/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\joaob\AppData\Local\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\joaob\OneDrive\Documentos\gestor_remoto\project"
+export "FLUTTER_APPLICATION_PATH=C:\Users\joaob\OneDrive\Documentos\GitHub\Gestor-Remoto\project"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=4.3.2"
