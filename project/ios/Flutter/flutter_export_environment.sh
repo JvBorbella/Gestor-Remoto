cac54cee726e8dcc4b0c08050d979db1fc214ff5
João Victor Borbella <116276996+JvBorbella@users.noreply.github.com>
2025-01-13 13:54:12 -0300

    Revert "Att 09/01 - 3"
    This reverts commit 489949676e1a443332de06f67c83e4e9f1dac58e.

--- project/ios/Flutter/flutter_export_environment.sh
+++ project/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\joaob\AppData\Local\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\joaob\OneDrive\Documentos\GitHub\Gestor-Remoto\project"
+export "FLUTTER_APPLICATION_PATH=C:\Users\joaob\OneDrive\Documentos\gestor_remoto\project"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
